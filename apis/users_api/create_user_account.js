17854ce3e5e4c234b7a9b26f3ecb0563b21a16c0
Aditya Chaudhary <adityachaudhary1029@gmail.com>
2023-03-21 02:48:31 +0530

    commit

--- apis/users_api/create_user_account.js
+++ apis/users_api/create_user_account.js
@@ -53,84 +53,84 @@ router.post("/login", async (req, res) => {
 // For account creation.
 
 router.post("/signup", async (req, res) => {
-  let voterIdNumber = req.body.voter_id_number;
-  let mobileNumber = req.body.mobile_number;
+  // let voterIdNumber = req.body.voter_id_number;
+  // let mobileNumber = req.body.mobile_number;
 
-  let result = await getUserDetails(voterIdNumber);
-  console.log("Result : ", result);
+  // let result = await getUserDetails(voterIdNumber);
+  // console.log("Result : ", result);
 
-  let response = {
-    status: 0,
-    message: "Account Created Sucessfully",
-    data: {
-      voter_id_number: voterIdNumber,
-      mobileNumber: mobileNumber,
-    },
-    election_details: {},
-  };
-  if (Object.keys(result).length === 0) {
+  // let response = {
+  //   status: 0,
+  //   message: "Account Created Sucessfully",
+  //   data: {
+  //     voter_id_number: voterIdNumber,
+  //     mobileNumber: mobileNumber,
+  //   },
+  //   election_details: {},
+  // };
+  // if (Object.keys(result).length === 0) {
     let response = await insertUserDetails(req.body);
 
-    const check = checkIfAlreadyVoted;
+  //   const check = checkIfAlreadyVoted;
 
-    const result = await check.getParticularCandidateDetail(voterIdNumber);
+  //   const result = await check.getParticularCandidateDetail(voterIdNumber);
 
-    if (Object.keys(result) === 0) {
-      res.send(response);
-    } else {
-      if (result[0]["is_vote_casted"] == true) {
-        response.status = 2;
-        response.message = "You have already casted voted";
-      } else {
-        const election = getElectionDetails.getElectionDetailsWithBooth;
+    // if (Object.keys(result) === 0) {
+    //   res.send(response);
+    // } else {
+    //   if (result[0]["is_vote_casted"] == true) {
+    //     response.status = 2;
+    //     response.message = "You have already casted voted";
+    //   } else {
+    //     const election = getElectionDetails.getElectionDetailsWithBooth;
 
-        let electionDetails = await election(result[0]["booth"]);
+    //     let electionDetails = await election(result[0]["booth"]);
 
-        console.log(result);
+    //     console.log(result);
 
-        response.data = electionDetails;
-      }
-    }
+    //     response.data = electionDetails;
+    //   }
+    // }
+
+  //   res.send(response);
+  // } else if (
+  //   result[0]["voter_id_number"] == voterIdNumber ||
+  //   result[0]["mobileNumber"] == mobileNumber
+  // ) {
+  //   response.status = 1;
+  //   response.message = "Account Already exist";
+
+  //   const check = checkIfAlreadyVoted;
+
+  //   const result = await check.getParticularCandidateDetail(voterIdNumber);
+
+    // if (Object.keys(result) === 0) {
+    //   res.send(response);
+    // } else {
+    //   if (result[0]["is_vote_casted"] == true) {
+    //     response.status = 2;
+    //     response.message = "You have already casted voted";
+    //   } else {
+    //     const election = getElectionDetails.getElectionDetailsWithBooth;
+
+    //     let electionDetails = await election(result[0]["booth"]);
+
+    //     console.log(electionDetails);
+
+    //     response.data = electionDetails;
+    //   }
+    // }
+
+  //   response.election_details = result;
+
+  //   res.send(response);
+  // } else {
+  //   response.status = 1;
+
+  //   response.message = "Something went wrong";
 
     res.send(response);
-  } else if (
-    result[0]["voter_id_number"] == voterIdNumber &&
-    result[0]["mobileNumber"] == mobileNumber
-  ) {
-    response.status = 1;
-    response.message = "Account Already exist";
-
-    const check = checkIfAlreadyVoted;
-
-    const result = await check.getParticularCandidateDetail(voterIdNumber);
-
-    if (Object.keys(result) === 0) {
-      res.send(response);
-    } else {
-      if (result[0]["is_vote_casted"] == true) {
-        response.status = 2;
-        response.message = "You have already casted voted";
-      } else {
-        const election = getElectionDetails.getElectionDetailsWithBooth;
-
-        let electionDetails = await election(result[0]["booth"]);
-
-        console.log(result);
-
-        response.data = electionDetails;
-      }
-    }
-
-    
-
-    res.send(response);
-  } else {
-    response.status = 1;
-
-    response.message = "Something went wrong";
-
-    res.send(response);
-  }
+  // }
 });
 
 // For updating user Details.
