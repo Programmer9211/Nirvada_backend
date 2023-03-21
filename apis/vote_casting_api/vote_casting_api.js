0ce775baf5a10b1e853b7825a04d3e64f9b56950
Aditya Chaudhary <adityachaudhary1029@gmail.com>
2023-03-21 07:52:35 +0530

    commit

--- apis/vote_casting_api/vote_casting_api.js
+++ apis/vote_casting_api/vote_casting_api.js
@@ -2,22 +2,31 @@ const express = require("express");
 const router = express.Router();
 const cadidateDetails = require("../../mongo_db/candidate_details_db/candidate_db_collection");
 const votingDetails = require("../../mongo_db/users_vote_details/users_vote_details_collection/update_details");
+const getCandidateDetails = require("../../mongo_db/candidate_details_db/candidate_details_db_operations/get_candidate_details");
 
 router.post("/insert", async (req, res) => {
-  const filter = { id: req.body.candidate_id };
-  const update = { $inc: { candidate_vote_count: 1 } };
-
   const collection = await cadidateDetails;
 
-  const result = await collection.findOneAndUpdate(filter, update);
+  const getVoteCount = await getCandidateDetails.getParticularCandidateWithId(
+    req.body.candidate_id
+  );
+
+  let voteCount = getVoteCount["candidate_vote_count"];
+
+  voteCount = voteCount + 1;
+
+  const filter = { id: req.body.candidate_id };
+  const update = { $set: { candidate_vote_count: voteCount } };
+
+  const result = await collection.updateOne(filter, update);
 
   const updateUserVotingStatus = await votingDetails(req.body.voter_id_number);
 
   res.send({
     data: {
-      "update_result": result,
-      "voting_status": updateUserVotingStatus,
-    }
+      update_result: result,
+      voting_status: updateUserVotingStatus,
+    },
   });
 });
 
